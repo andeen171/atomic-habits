34b3cf2a31203268fb9e4ba46a9dd2d1dcafeea7
Vitor Efraim <vitgabriel.dev@gmail.com>
2025-06-22 23:58:43 -0300

    wip feat: make buttons work like tab selectors when on smaller screens

--- apps/web/src/components/navigation.tsx
+++ apps/web/src/components/navigation.tsx
@@ -1,19 +1,27 @@
 import { Link } from "@tanstack/react-router";
 
 import { Button } from "@/components/ui/button";
+import { HomeIcon } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
 import UserMenu from "./user-menu";
 
 export default function Navigation() {
 	const links = [{ to: "/", label: "Todos" }];
+	const isActive = (to: string) => location.pathname === to;
 
 	return (
 		<div className="fixed bottom-0 left-0 z-50 w-full sm:static sm:top-auto sm:bottom-auto">
-			<div className="flex flex-row items-center justify-between border-b bg-background px-2 py-1">
-				<nav className="flex gap-4 text-lg">
+			<div className="flex flex-row items-center justify-between border-t bg-background px-2 py-3 sm:border-b sm:py-1">
+				<nav className="flex gap-4 text-2xl">
 					{links.map(({ to, label }) => (
-						<Link key={to} to={to}>
-							{label}
+						<Link key={to} to={to} className="h-full w-full">
+							<Button
+								variant={isActive(to) ? "default" : "ghost"}
+								className="flex min-w-[80px] flex-col items-center gap-0 py-7 sm:py-5.5"
+							>
+								<HomeIcon />
+								{label}
+							</Button>
 						</Link>
 					))}
 				</nav>
